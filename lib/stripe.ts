6319869201cab6170299d94a9a8db97601c62310
Product Sniper <firebeatsz21@gmail.com>
2026-03-09 12:11:12 +0100

    remove stripe apiVersion to fix typescript build error

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,10 +1,9 @@
 import Stripe from "stripe";
 
 const secretKey = process.env.STRIPE_SECRET_KEY;
+
 if (!secretKey) {
   throw new Error("Missing STRIPE_SECRET_KEY environment variable");
 }
 
-export const stripe = new Stripe(secretKey, {
-  apiVersion: "2024-06-20",
-});
+export const stripe = new Stripe(secretKey);
